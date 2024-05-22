8006c8bf98bb9b6056f28dc421ee8cd50d71fce3
chip902 <68761150+chip902@users.noreply.github.com>
2024-05-21 23:49:45 -0400

    set up db connection

---
+++ app/api/issues/route.ts
@@ -0,0 +1,19 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const schema = z.object({
+	first_name: z.string().min(1).max(255),
+	last_name: z.string().min(1).max(255),
+	email: z.string().email(),
+});
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+	const validation = schema.safeParse(body);
+	if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
+	const newUser = await prisma.user.create({
+		data: { first_name: body.first_name, last_name: body.last_name, email: body.email },
+	});
+	return NextResponse.json(newUser, { status: 201 });
+}
