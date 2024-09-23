dceee172a01159fce84fc68faeaffbf2602a5809
chip902 <68761150+chip902@users.noreply.github.com>
2024-09-23 00:44:38 -0400

    deps update and Texteditor updates

--- app/admin/newsletter/CustomEditor.tsx
+++ app/admin/newsletter/CustomEditor.tsx
@@ -1,19 +1,25 @@
-"use client"; // only in App Router
-
+import React from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import { Bold, Essentials, Italic, Mention, Paragraph, Undo } from "ckeditor5";
-import ClassicEditor from "./ckeditor";
-import "./editor.css";
-import "./ckeditor";
+import ClassicEditorBase from "@ckeditor/ckeditor5-build-classic";
 
-function CustomEditor() {
+// Extend the classic editor with additional features if needed
+class CustomClassicEditor extends ClassicEditorBase {}
+
+const CustomEditor: React.FC = () => {
 	return (
 		<CKEditor
-			editor={ClassicEditor}
-			
+			editor={CustomClassicEditor}
+			data="<p>Hello from CKEditor 5!</p>"
+			onReady={(editor: any) => {
+				// You can store the "editor" and use when it is needed.
+				console.log("Editor is ready to use!", editor);
+			}}
+			onChange={(event: any, editor: { getData: () => any }) => {
+				const data = editor.getData();
+				console.log({ event, editor, data });
 			}}
 		/>
 	);
-}
+};
 
 export default CustomEditor;
--- app/admin/newsletter/TextEditor.tsx
+++ app/admin/newsletter/TextEditor.tsx
@@ -1,58 +1,152 @@
 "use client";
-import { ElementType, useState } from "react";
-import { Box, Textarea, Button, Select } from "@chakra-ui/react";
-import React from "react";
+import React, { useRef, useState } from "react";
+import ContentEditable from "react-contenteditable";
+import { Box, Button, Select, HStack } from "@chakra-ui/react";
 
 export default function TextEditor() {
+	const textEditorRef = useRef<HTMLDivElement>(null);
 	const [text, setText] = useState("");
-	const [fontFamily, setFontFamily] = useState("Arial");
+	const [fontFamily, setFontFamily] = useState("Quicksand");
 	const [fontSize, setFontSize] = useState(16);
-	const [headingLevel, setHeadingLevel] = useState<number>(1);
+	const [isBold, setIsBold] = useState(false);
+	const [isItalic, setIsItalic] = useState(false);
+	const [isUnderline, setIsUnderline] = useState(false);
 
-	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		setText(e.target.value);
+	const handleChange = (evt: React.FormEvent<HTMLDivElement>) => {
+		setText(evt.currentTarget.innerHTML);
+	};
+
+	// Apply styles to the selected text and update the state
+	const applyStyleToSelection = (style: string, state: boolean) => {
+		if (!textEditorRef.current) return;
+
+		const selection = window.getSelection();
+		if (!selection || !selection.rangeCount) return;
+		const range = selection.getRangeAt(0);
+		let styledText: Node | null;
+
+		switch (style) {
+			case "bold":
+				styledText = document.createElement("strong");
+				setIsBold(state);
+				break;
+			case "italic":
+				styledText = document.createElement("em");
+				setIsItalic(state);
+				break;
+			case "underline":
+				styledText = document.createElement("u");
+				setIsUnderline(state);
+				break;
+			default:
+				return;
+		}
+
+		if (styledText) {
+			while (range.startContainer.firstChild && !range.collapsed) {
+				const node = range.startContainer.removeChild(range.startContainer.firstChild);
+				styledText.appendChild(node);
+			}
+
+			range.insertNode(styledText);
+		} else {
+			console.error("Failed to create element for style:", style);
+		}
+	};
+
+	// Apply font family to the selected text and update the state
+	const applyFontFamilyToSelection = (fontFamily: string) => {
+		if (!textEditorRef.current) return;
+
+		const selection = window.getSelection();
+		if (!selection || !selection.rangeCount) return;
+		const range = selection.getRangeAt(0);
+		const span = document.createElement("span");
+		span.style.fontFamily = fontFamily;
+		span.appendChild(range.extractContents());
+		range.insertNode(span);
+		setFontFamily(fontFamily);
+	};
+
+	// Apply font size to the selected text and update the state
+	const applyFontSizeToSelection = (fontSize: number) => {
+		if (!textEditorRef.current) return;
+
+		const selection = window.getSelection();
+		if (!selection || !selection.rangeCount) return;
+		const range = selection.getRangeAt(0);
+		const span = document.createElement("span");
+		span.style.fontSize = `${fontSize}px`;
+		span.appendChild(range.extractContents());
+		range.insertNode(span);
+		setFontSize(fontSize);
+	};
+	const getStyleObject = (bold: boolean, italic: boolean, underline: boolean) => {
+		return {
+			minHeight: "200px",
+			border: "1px solid #ccc",
+			padding: "8px",
+			fontFamily,
+			fontSize,
+			fontWeight: bold ? "bold" : "normal",
+			fontStyle: italic ? "italic" : "normal",
+			textDecoration: underline ? "underline" : "none",
+		};
 	};
 
 	return (
-		<Box p={5} shadow="md" borderWidth="1px">
-			<Select value={fontFamily} onChange={(e) => setFontFamily(e.target.value)} mb={4}>
-				<option value="Arial">Arial</option>
-				<option value="Courier New">Courier New</option>
-				<option value="Quicksand">Quicksand</option>
-				<option value="Times New Roman">Times New Roman</option>
-			</Select>
+		<>
+			<Box>
+				<HStack spacing={4} mb={4}>
+					<Select value={fontFamily} onChange={(e) => applyFontFamilyToSelection(e.target.value)}>
+						<option value="Arial">Arial</option>
+						<option value="Courier New">Courier New</option>
+						<option value="Quicksand">Quicksand</option>
+						<option value="Times New Roman">Times New Roman</option>
+					</Select>
 
-			<Select value={fontSize} onChange={(e) => setFontSize(Number(e.target.value))} mb={4}>
-				{[12, 14, 16, 18, 20, 24, 30].map((size) => (
-					<option key={size} value={size}>
-						{size}
-					</option>
-				))}
-			</Select>
+					<Select value={fontSize} onChange={(e) => applyFontSizeToSelection(Number(e.target.value))}>
+						{[12, 14, 16, 18, 20, 24, 30].map((size) => (
+							<option key={size} value={size}>
+								{size}
+							</option>
+						))}
+					</Select>
 
-			<Select value={headingLevel} onChange={(e) => setHeadingLevel(Number(e.target.value))} mb={4}>
-				<option value="">None</option>
-				{[1, 2, 3, 4, 5, 6].map((level) => (
-					<option key={level} value={String(level)}>
-						Heading {level}
-					</option>
-				))}
-			</Select>
+					<Button onClick={() => applyStyleToSelection("bold", isBold ? false : true)} variant={isBold ? "solid" : "ghost"} colorScheme="orange">
+						Bold
+					</Button>
+					<Button
+						onClick={() => applyStyleToSelection("italic", isItalic ? false : true)}
+						variant={isItalic ? "solid" : "ghost"}
+						colorScheme="orange">
+						Italic
+					</Button>
+					<Button
+						onClick={() => applyStyleToSelection("underline", isUnderline ? false : true)}
+						variant={isUnderline ? "solid" : "ghost"}
+						colorScheme="orange">
+						Underline
+					</Button>
+				</HStack>
 
-			<Textarea
-				value={text}
-				onChange={handleChange}
-				placeholder="Start typing..."
-				size="lg"
-				resize="none"
-				mb={4}
-				fontFamily={fontFamily}
-				fontSize={`${fontSize}px`}
-			/>
+				<ContentEditable
+					html={text}
+					onChange={handleChange}
+					tagName="div"
+					style={getStyleObject(isBold, isItalic, isUnderline)}
+					innerRef={textEditorRef}
+				/>
 
-			<Button colorScheme="blue" onClick={() => console.log(text)}>
-				Save Text
-			</Button>
-		</Box>
+				<Button mt={4} colorScheme="blue" onClick={() => console.log(text)}>
+					Save Text
+				</Button>
+			</Box>
+
+			<Box mt={4}>
+				<h2>Preview</h2>
+				<div dangerouslySetInnerHTML={{ __html: text }} style={getStyleObject(isBold, isItalic, isUnderline)} />
+			</Box>
+		</>
 	);
 }
